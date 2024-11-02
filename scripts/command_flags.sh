f772c59299cadbe3d75f6e1c0278a4ba775849e0
Jamie Land <hokie10@gmail.com>
2024-11-01 20:40:46 -0400

    Adding reset to the cli flags

--- scripts/command_flags.sh
+++ scripts/command_flags.sh
@@ -10,6 +10,7 @@ function show_help {
   echo "  --bootstrap_dir=<bootstrap_directory>    Base folder inside of bootstrap/overlays (Optional, pick during script execution if not set)"
   echo "  --timeout=45          Timeout in seconds for waiting for each resource to be ready"
   echo "  -f                    If set, will update the `patch-application-repo-revision` folder inside of your overlay with the current git information and push a checkin"
+  echo "  --reset-git           Locally resets changes made by the bootstrap script. Please run and checkin the changes before creating a PR"
   echo "  --help                Show this help message"
 }
 
@@ -17,32 +18,36 @@ for arg in "$@"
 do
   case $arg in
     --base_domain=*)
-    export BASE_DOMAIN="${arg#*=}"
-    shift
+      export BASE_DOMAIN="${arg#*=}"
+      shift
     ;;
     --ocp_version=*)
     export OCP_VERSION="${arg#*=}"
-    echo "Using OCP Bianaires Version: ${OCP_VERSION}"
-    shift
+      echo "Using OCP Bianaires Version: ${OCP_VERSION}"
+      shift
     ;;
     --bootstrap_dir=*)
     export BOOTSTRAP_DIR="${arg#*=}"
-    echo "Using Bootstrap Directory: ${BOOTSTRAP_DIR}"
-    shift
+      echo "Using Bootstrap Directory: ${BOOTSTRAP_DIR}"
+      shift
     ;;
     -f)
     export FORCE=true
-    echo "Force set, using current git branch"
-    shift
+      echo "Force set, using current git branch"
+      shift
     ;;
     -update_base_domain)
-    export UPDATE_BASEDOMAIN=true
-    echo "Update Base Domain set, will update the base domain on the git repo"
-    shift
+      export UPDATE_BASEDOMAIN=true
+      echo "Update Base Domain set, will update the base domain on the git repo"
+      shift
+    ;;
+    --reset-git)
+      source "$(dirname "$0")/reset_git.sh"
+      exit 0
     ;;
     --help)
-    show_help
-    exit 0
+      show_help
+      exit 0
     ;;
   esac
 done
